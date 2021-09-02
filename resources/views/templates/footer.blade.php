5fe2f9795cfd91c7a438d42bd99d90a8a04dd2bb
Gurdeep Singh <gurdeepsingh2000329@gmail.com>
2021-09-02 17:40:03 +0200

    aggiunto footer

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,3 +1,83 @@
 <div class="background-footer">
-    
+            <div class = 'footer-menu'>
+                <ul>
+                    <h2>DC COMICS</h2>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>Tv</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+            
+                <ul>
+                    <h2>DC</h2>
+                    <li>Terms of Use</li>
+                    <li>Privacy Polics (New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshop</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+
+                <ul>
+                    <h2>SITES</h2>
+                    <li>DC</li>
+                    <li>MAD MagazineE</li>
+                    <li>DC KIDS</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+
+                <ul>
+                    <h2>SHOP</h2>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+
+            </div>
+
+            <div class = 'footer-logo'>
+                <div class = 'logo-container'>
+                    <img src="/../../images/dc-logo-bg.png" alt="">
+                </div>
+            </div>
+
+    </div>
+
+
+        <div class="social-media">
+            <div><button>SIGN UP NOW!</button></div>
+
+            <div class="social-icons-container">
+                <span>FOLLOW US!</span>
+
+                <div class="social-icon">
+                    <img src="/../../images/footer-facebook.png" alt="">
+                </div>
+
+                <div class="social-icon">
+                    <img src="/../../images/footer-twitter.png" alt="">
+                </div>
+
+                <div class="social-icon">
+                    <img src="/../../images/footer-youtube.png" alt="">
+                </div>
+
+                <div class="social-icon">
+                    <img src="/../../images/footer-pinterest.png" alt="">
+                </div>
+
+                <div class="social-icon">
+                    <img src="/../../images/footer-periscope.png" alt="">
+                </div>
+            </div>
+
+        </div>
 </div>
